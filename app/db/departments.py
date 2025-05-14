13aa599e7213d9a04787ec1fa732fb4b2d24a921
Thanh-WuTan <wt.vathanh@gmail.com>
2025-05-14 20:33:41 +0700

    feat: query departments

---
+++ app/db/departments.py
@@ -0,0 +1,18 @@
+from .con import get_db_connection
+from mysql.connector import Error
+
+def get_all_departments():
+    connection = get_db_connection()
+    if connection is None:
+        return []
+    try:
+        cursor = connection.cursor()
+        cursor.execute("SELECT id, department_name FROM departments")
+        departments = cursor.fetchall()
+        return departments
+    except Error as e:
+        print(f"Error fetching departments: {e}")
+        return []
+    finally:
+        cursor.close()
+        connection.close()
