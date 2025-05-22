6be27cbe5fa10e309ad7d94d6779fc19a45c9774
Thanh-WuTan <wt.vathanh@gmail.com>
2025-05-22 19:50:49 +0700

    use role required and remove unused import, functions

--- app/instructor.py
+++ app/instructor.py
@@ -1,15 +1,13 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_user, current_user, login_required
+from app.role_required import role_required
 from app.service.auth_service import authenticate_user
-from app.service.instructor_service import get_all_courses_from_instructor_service, get_course_by_instructor_id
+from app.service.instructor_service import get_course_by_instructor_id
 from app.service.admin_service import (
-    get_all_courses_service, delete_course_service, get_all_users_service, 
-    delete_user_service, add_user_service, edit_user_service, add_course_service, 
-    edit_course_service, get_course_details_service, get_user_role_counts_service, 
-    get_course_department_counts_service, get_student_department_counts_service, get_available_students_service, enroll_student_service, unenroll_student_service
+    get_course_details_service,  
+    get_available_students_service, 
+    enroll_student_service, unenroll_student_service
 )
-from app.forms import CreateUserForm, UpdateUserForm, CourseForm, CourseInstructorForm
-
 from app.db.users import User
 
 instructor = Blueprint('instructor', __name__)
@@ -35,27 +33,14 @@ def instructor_login():
 
 @instructor.route('/dashboard')
 @login_required
+@role_required('instructor')
 def dashboard():
-    if current_user.role != 'instructor':
-        flash('Unauthorized access.', 'danger')
-        return redirect(url_for('main.index'))
     return render_template('instructor/dashboard.html', title='Instructor Dashboard')
 
 @instructor.route('/dashboard/courses/', methods=['GET', 'POST'])
 @login_required
+@role_required('instructor')
 def courses():
-    if current_user.role != 'instructor':
-        flash('Unauthorized access.', 'danger')
-        return redirect(url_for('main.index'))
-    # if request.method == 'POST':
-    #     course_id = request.form.get('course_id')
-    #     if course_id:
-    #         try:
-    #             delete_course_service(course_id)
-    #             flash('Course deleted successfully!', 'success')
-    #         except Exception as e:
-    #             flash(f'Error deleting course: {str(e)}', 'danger')
-    #         return redirect(url_for('admin.courses'))
     instructor_id = current_user.id
     courses = get_course_by_instructor_id(instructor_id)
     print("Fetched courses:", courses)  # DEBUG LINE
@@ -63,11 +48,8 @@ def courses():
 
 @instructor.route('/dashboard/courses/<int:course_id>')
 @login_required
+@role_required('instructor')
 def course_detail(course_id):
-    if current_user.role != 'instructor':
-        flash('Unauthorized access.', 'danger')
-        return redirect(url_for('main.index'))
-    
     course, enrolled_students = get_course_details_service(course_id)
     available_students = get_available_students_service(course_id)
 
@@ -81,11 +63,8 @@ def course_detail(course_id):
 
 @instructor.route('/dashboard/courses/<int:course_id>/enroll', methods=['POST'])
 @login_required
+@role_required('instructor')
 def enroll_student(course_id):
-    if current_user.role != 'instructor':
-        flash('Unauthorized access.', 'danger')
-        return redirect(url_for('instructor.course_detail', course_id=course_id))
-    
     student_id = request.form.get('student_id')
     if student_id:
         try:
@@ -98,73 +77,10 @@ def enroll_student(course_id):
 
 @instructor.route('/dashboard/courses/<int:course_id>/unenroll/<int:student_id>', methods=['POST'])
 @login_required
+@role_required('instructor')
 def unenroll_student(course_id, student_id):
-    if current_user.role != 'instructor':
-        flash('Unauthorized access.', 'danger')
-        return redirect(url_for('instructor.course_detail', course_id=course_id))
-    
     try:
         unenroll_student_service(student_id, course_id)
     except Exception as e:
         flash(f'Error unenrolling student: {str(e)}', 'danger')
     return redirect(url_for('instructor.course_detail', course_id=course_id))
-
-@instructor.route('/dashboard/courses/add', methods=['POST'])
-@login_required
-def add_course():
-    if current_user.role != 'instructor':
-        flash('Unauthorized access', 'danger')
-        return redirect(url_for('admin.courses'))
-    users = get_all_users()
-    instructors = [(user['id'], user['username']) for user in users if user['role_name'] == 'instructor']
-    form = CourseForm()
-    form.instructor_id.choices = instructors
-    if form.validate_on_submit():
-        course_name = form.course_name.data
-        department_id = form.department_id.data
-        instructor_id = form.instructor_id.data
-        location = form.location.data
-        schedule = form.schedule.data
-        semester = form.semester.data
-        availability = form.availability.data
-        try:
-            add_course_service(course_name, department_id, instructor_id, location, schedule, semester, availability)
-            flash('Course added successfully', 'success')
-            return redirect(url_for('instructor.courses'))
-        except Exception as e:
-            flash(f'Error adding course: {str(e)}', 'danger')
-    else:
-        for field, errors in form.errors.items():
-            for error in errors:
-                flash(f'Error in {field}: {error}', 'danger')
-    return redirect(url_for('instructor.courses'))
-
-@instructor.route('/dashboard/courses/edit/<int:course_id>', methods=['POST'])
-@login_required
-def edit_course(course_id):
-    if current_user.role != 'instructor':
-        flash('Unauthorized access', 'danger')
-        return redirect(url_for('instructor.courses'))
-    users = get_all_users()
-    instructors = [(user['id'], user['username']) for user in users if user['role_name'] == 'instructor']
-    form = CourseForm()
-    form.instructor_id.choices = instructors
-    if form.validate_on_submit():
-        course_name = form.course_name.data
-        department_id = form.department_id.data
-        instructor_id = form.instructor_id.data
-        location = form.location.data
-        schedule = form.schedule.data
-        semester = form.semester.data
-        availability = form.availability.data
-        try:
-            edit_course_service(course_id, course_name, department_id, instructor_id, location, schedule, semester, availability)
-            flash('Course updated successfully', 'success')
-            return redirect(url_for('instructor.courses'))
-        except Exception as e:
-            flash(f'Error updating course: {str(e)}', 'danger')
-    else:
-        for field, errors in form.errors.items():
-            for error in errors:
-                flash(f'Error in {field}: {error}', 'danger')
-    return redirect(url_for('instructor.courses'))
