5de3db3e24e5c6cda6f151b51ec3f77eb4f6ce18
Thanh-WuTan <wt.vathanh@gmail.com>
2025-05-22 19:55:48 +0700

    remove unused imports

--- app/service/instructor_service.py
+++ app/service/instructor_service.py
@@ -1,4 +1,4 @@
-from app.db.courses import get_filtered_courses, db_get_course_by_instructor_id
+from app.db.courses import db_get_course_by_instructor_id
 
 def get_course_by_instructor_id(instructor_id: int):
     return db_get_course_by_instructor_id(instructor_id)
