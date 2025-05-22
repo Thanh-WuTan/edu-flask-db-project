2a8f754064a0aee9b89e610cea4bdfc34f5cce2d
Thanh-WuTan <wt.vathanh@gmail.com>
2025-05-22 19:54:46 +0700

    fix

--- app/service/instructor_service.py
+++ app/service/instructor_service.py
@@ -1,8 +1,5 @@
 from app.db.courses import get_filtered_courses, db_get_course_by_instructor_id
 
-def get_all_courses_from_instructor_service(instructor_id: int):
-    return get_filtered_courses(instructor_id=instructor_id)
-
 def get_course_by_instructor_id(instructor_id: int):
     return db_get_course_by_instructor_id(instructor_id)
 
